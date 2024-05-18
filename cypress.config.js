93ea06e092e66a69e774520d8100c92a46f2859b
Daniel Oliveira <danieldfs18@hotmail.com>
2024-05-18 19:57:08 -0300

    api test shazam

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  e2e: {
+    failOnStatusCode: false,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    env:{
+      url: "https://shazam.p.rapidapi.com/artists/get-top-songs",
+      apiKey: "c6cfae4770mshfa1364e50bc1a23p11c49bjsn88f8e97d46c0",
+      host: "shazam.p.rapidapi.com"
+    }
+  },
+});
---
+++ cypress/e2e/shazam-api.cy.js
@@ -0,0 +1,114 @@
+import 'cypress-mochawesome-reporter/register';
+
+describe('Test /artist/get-top-songs from Shazam', () => {
+  const BASEURL = Cypress.env('url');
+  const apiKey = Cypress.env('apiKey');
+  const host = Cypress.env('host');
+
+  const headers = {
+    'X-RapidAPI-Key': apiKey,
+    'X-RapidAPI-Host': host
+  };
+  const qsValidId = {
+    id: 567072,
+    l: "en-US",
+  };
+  const qsInvalidId = {
+    id: 5670729990000,
+    l: "en-US",
+  };
+
+  it('Verify the get top songs endpoint returns 200', () => {
+    cy.request({
+      method: 'GET',
+      url: BASEURL,
+      qs: qsValidId,
+      headers: headers
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it('Verify the get top songs endpoint returns 10 songs', () => {
+    cy.request({
+      method: 'GET',
+      url: BASEURL,
+      qs: qsValidId,
+      headers: headers
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+
+      const songs = response.body.data;
+      cy.log(songs);
+      expect(songs).to.be.an('array').that.has.lengthOf(10);
+    });
+  });
+
+  it('Verify a 404 status code is returned when the artistId is invalid', () => {
+    cy.request({
+      method: 'GET',
+      url: BASEURL,
+      qs: qsInvalidId,
+      headers: headers,
+      failOnStatusCode: false // Permitimos continuar mesmo com status 404
+    }).should((response) => {
+      const errors = response.body.errors;
+      expect(errors).to.be.an('array').that.is.not.empty;
+      expect(errors[0].status).to.equal("404");
+    });
+  });
+
+  it('Verify the get top songs endpoint returns 403 when the API key is invalid', () => {
+    const invalidHeaders = {
+      ...headers,
+      'X-RapidAPI-Key': 'c6cfae4770mshfa1364e50bc1a23p11c49bjsn88f8e97d46c0sdsds'
+    };
+
+    cy.request({
+      method: 'GET',
+      url: BASEURL,
+      qs: qsValidId,
+      headers: invalidHeaders,
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.equal(403);
+      expect(response.body.message).to.equal("You are not subscribed to this API.");
+    });
+  });
+
+  it('Verify the type attribute of the first object is returning as "Song"', () => {
+    cy.request({
+      method: 'GET',
+      url: BASEURL,
+      qs: qsValidId,
+      headers: headers
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+
+      const songs = response.body.data;
+      expect(songs).to.be.an('array').that.has.lengthOf(10);
+
+      songs.forEach(song => {
+        expect(song.type).to.equal("songs");
+      });
+    });
+  });
+
+  it('Verify the artist name is Gorillaz', () => {
+    cy.request({
+      method: 'GET',
+      url: BASEURL,
+      qs: qsValidId,
+      headers: headers
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+
+      const songs = response.body.data;
+      expect(songs).to.be.an('array').that.has.lengthOf(10);
+
+      songs.forEach(song => {
+        expect(song.attributes.artistName).to.equal("Gorillaz");
+      });
+    });
+  });
+});
